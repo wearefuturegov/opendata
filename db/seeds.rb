26f0640e086a4f0f02c8bf7dc4c746b1c9df7b68
Andrew Sprinz <andrewsprinz@gmail.com>
2015-05-13 08:45:17 +0100

    Basic areas model and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+areas = Area.create([{ name: 'Devon' }, { name: 'Kingston' }, { name: 'Sutton' }])
