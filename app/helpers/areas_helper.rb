6bc37c3de842fab3c054873fd4cf7a924ad1c1c4
LkeMitchll <lke.mitchll@gmail.com>
2015-06-01 13:13:00 +0100

    Make the chart header background dynamic:
    Add a helper to determine if an audience is present and construct an
    HTML friendly ID to change the background colour via CSS.

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -1,2 +1,9 @@
 module AreasHelper
+  # Helper to determine which audience is selected and create an HTML friendly ID 
+  # Enables us to change the background color of the chart header
+  def chart_heading_id
+    if @audience.present?
+      return "tab_" + @audience.title.parameterize
+    end
+  end
 end
