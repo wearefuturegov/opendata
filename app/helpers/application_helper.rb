5633617c23219af65f1a51e4cef18b02c7075a46
LkeMitchll <lke.mitchll@gmail.com>
2015-06-09 15:33:26 +0100

    Add links to download files on homepage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def static_file(url)
+    filename = url.rpartition('/').last
+    extension = filename.to_s.rpartition('.').last
+
+    link_to(url, target: '_blank', class: 'document-link') do
+      capture do
+        concat content_tag(:span, "", class: 'icon icon-document-1')
+        concat content_tag(:span, extension.upcase, class: 'document-type ' + extension)
+        concat filename
+      end
+    end
+  end
 end
